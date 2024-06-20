eb44f5e92ca3eb42666de7d9a75dffe0e506ab42
Kyle Murphy <kmurph73@gmail.com>
2024-06-20 12:54:06 -0700

    try docs folder

--- build.js
+++ build.js
@@ -1,14 +1,14 @@
 import fs from 'node:fs/promises';
 
 try {
-	await fs.rm('dist', {recursive: true, force: true});
+	await fs.rm('docs', {recursive: true, force: true});
 } catch {
 }
 
-await fs.mkdir("dist");
+await fs.mkdir("docs");
 
 let html = await fs.readFile('app/index.html', 'utf8');
 
 html = html.replace("/index.js", "/kcapp/index.js");
 
-await fs.writeFile('dist/index.html', html);
+await fs.writeFile('docs/index.html', html);
